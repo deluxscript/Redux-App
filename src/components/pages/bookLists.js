ad34da29fb732786ff0c99b6ae4b7c41f6a3b2a1
deluxscript <delsam94@yahoo.com>
2017-12-07 20:16:55 -0800

    cool

--- src/components/pages/bookLists.js
+++ src/components/pages/bookLists.js
@@ -2,24 +2,32 @@
 
 import React from "react";
 import { connect } from "react-redux";
+import { Grid, Row, Button, Col, Well } from "react-bootstrap";
 
 class BookLists extends React.Component{
     render() {
         console.log('Accessing: ', this.props.books);
         const bookLists = this.props.books.map(function(booksArr){
             return(
-                <div key={booksArr.id}>
-                    <h1>{booksArr.Title}</h1>
-                    <p>{booksArr.Description}</p>
-                    <p>{booksArr.Category}</p>
-                    <p>{booksArr.Price}</p>
-                </div>
+                <Col md={4}  key={booksArr.id}>
+                    <Well>
+                        <h1>{booksArr.Title}</h1>
+                        <p>{booksArr.Description}</p>
+                        <p>{booksArr.Category}</p>
+                        <p>{booksArr.Price}</p>
+                        <Button bsStyle='primary'>Buy Now</Button>
+                    </Well>
+                </Col>
             )
         })
         return (
           <div>
             <h2>Book Lists</h2>
-            {bookLists}
+            <Grid>
+                <Row>
+                    {bookLists}
+                </Row>
+            </Grid>
           </div>
         );
       }
@@ -30,5 +38,8 @@ function mapStateToProps (state)  {
         books: state.books.books
     }
 }
+function foo () { return 5;}
+var myVar = foo;
+console.log(myVar);
 
 export default connect(mapStateToProps) (BookLists);
